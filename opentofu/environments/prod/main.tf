8b12398a8cb62dfe320a77c0c76c46566cf561f0
Brian Kurilko <briankurilko@gmail.com>
2026-08-14 16:23:07 -0400

    Small reformat

--- opentofu/environments/prod/main.tf
+++ opentofu/environments/prod/main.tf
@@ -1,8 +1,8 @@
 module "infra" {
   source = "../../modules/infra"
 
-  first_file_name    = "prod_art_of_war.txt"
-  second_file_name   = "prod_another_art.txt"
-  first_file_content = "sup it's brian on prod"
+  first_file_name     = "prod_art_of_war.txt"
+  second_file_name    = "prod_another_art.txt"
+  first_file_content  = "sup it's brian on prod"
   second_file_content = "sup it's brian on prod again how's it going"
 }
